92762ad71f51ea078ea445e4523e84e63f723a2c
CSZQCWink <switchwink@163.com>
2023-07-19 20:27:53 +0800

    first commit

---
+++ SSM1/src/main/java/com/csz/ssm/GitTest.java
@@ -0,0 +1,15 @@
+package com.csz.ssm;
+
+/**
+ * @BelongsPackage: com.csz.ssm
+ * @ClassName: GitTest
+ * @Author: QC_Wink
+ * @Description: git测试
+ * @CreateTime: 2023-07-19  20:17
+ * @Version: 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
