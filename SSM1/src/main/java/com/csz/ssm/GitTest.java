9ede41b61093c563c7729472d2be442c39cfc83d
CSZQCWink <switchwink@163.com>
2023-07-20 23:14:47 +0800

    20230720-2314 commit

--- SSM1/src/main/java/com/csz/ssm/GitTest.java
+++ SSM1/src/main/java/com/csz/ssm/GitTest.java
@@ -11,5 +11,6 @@ package com.csz.ssm;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("我在笔记本上正常的进行了克隆 并且成功运行代码");
+        System.out.println("测试是否可以正常推送到GitHub");
     }
 }
