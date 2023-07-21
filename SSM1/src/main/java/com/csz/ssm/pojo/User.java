f1fc5cf5e54a7c3f6dc2a0467abd5192af8f693b
CSZQCWink <switchwink@163.com>
2023-07-21 16:17:18 +0800

    NB commit 20230721-1616

--- SSM1/src/main/java/com/csz/ssm/pojo/User.java
+++ SSM1/src/main/java/com/csz/ssm/pojo/User.java
@@ -17,10 +17,16 @@ import lombok.*;
 @Setter
 @ToString
 public class User {
+    // 用户id
     private Integer id;
+    // 用户名称
     private String name;
+    // 用户密码
     private String password;
+    // 用户年龄
     private int age;
+    // 用户性别
     private String gender;
+    // 用户邮箱
     private String email;
 }
--- SSM1/src/test/java/MyBatisTest.java
+++ SSM1/src/test/java/MyBatisTest.java
@@ -12,19 +12,32 @@ public class MyBatisTest {
     public void testInsert() throws IOException {
         // 获取核心配置文件的输入流
         InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
+
         // 获取SqlSessionFactoryBuilder对象
         SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+
         // 获取SqlSessionFactory对象
         SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(resourceAsStream);
+
         // 获取SqlSession对象 是MyBatis提供的操作数据库的对象 添加true参数就会自动提交
         SqlSession sqlSession = sqlSessionFactory.openSession(true);
+
         // 获取UserMapper接口的代理实现类
         UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
         // 调用mapper接口中的方法实现添加用户信息的功能
         int result = mapper.insertUser();
-        System.out.println(result);
+
+        // 判断是否添加成功
+        if(result == 1){
+            System.out.println("添加用户成功："+result);
+        }else {
+            System.out.println("添加用户失败："+result);
+        }
+
         // 提交事务
         // sqlSession.commit();
+
         // 关闭会话
         sqlSession.close();
     }
